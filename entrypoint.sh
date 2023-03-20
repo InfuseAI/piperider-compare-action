7b6d6331190f8a3182d8a072b84e4c60e44aa99a
Ching Yi, Chan <qrtt1@infuseai.io>
2023-03-21 07:56:26 +0800

    wip
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,10 @@ fi
 git config --global --add safe.directory /github/workspace
 piperider compare ; rc=$?
 
-echo "::set-output name=analysis::${log}"
 echo "::set-output name=status::${rc}"
 echo "::set-output name=uuid::${uuid}"
 
-pushd /usr/src/github-action
-/root/.nvm/versions/node/v16.13.0/bin/node index.js $rc || exit $?
-popd
+# TODO enable it after compare working
+# pushd /usr/src/github-action
+# /root/.nvm/versions/node/v16.13.0/bin/node index.js $rc || exit $?
+# popd
